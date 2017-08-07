fbd1b3bb89718a7aac184234eddd396c577b93d3
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-08-07 20:21:23 -0300

    Testing heroku.

--- CodaMais/CodaMais/settings.py
+++ CodaMais/CodaMais/settings.py
@@ -105,6 +105,7 @@ WSGI_APPLICATION = 'CodaMais.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
+
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # User model
