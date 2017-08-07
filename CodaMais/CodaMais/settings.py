139d2332bb962ea047b4ed6db79fa20eb982dc69
Ronyell Henrique dos Santos <ronyellhenrique@gmail.com>
2017-08-07 20:03:43 -0300

    Correcting database.

--- CodaMais/CodaMais/settings.py
+++ CodaMais/CodaMais/settings.py
@@ -103,6 +103,8 @@ WSGI_APPLICATION = 'CodaMais.wsgi.application'
 # Database.
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # User model
